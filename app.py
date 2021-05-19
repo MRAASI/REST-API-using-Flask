0c6637ac5fac9f42cc4710ea270bf408af54c2fe
Raasi Mathi <46440771+MRAASI@users.noreply.github.com>
2021-05-19 11:48:08 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,78 @@
+import flask
+from flask import request, jsonify
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+# Create some test data for our catalog in the form of a list of dictionaries.
+books = [
+    {'id': 0,
+     'title': 'A Fire Upon the Deep',
+     'author': 'Vernor Vinge',
+     'published': '1992'},
+    {'id': 1,
+     'title': 'The Ones Who Walk Away From Omelas',
+     'author': 'Ursula K. Le Guin',
+     'published': '1973'},
+    {'id': 2,
+     'title': 'Dhalgren',
+     'author': 'Samuel R. Delany',
+     'published': '1975'}
+]
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return '''<h1>Distant Reading Archive</h1>
+<p>A prototype API for distant reading of science fiction novels.</p>'''
+
+
+# A route to return all of the available entries in our catalog.
+
+@app.route('/all', methods=['GET'])
+def api_all():
+    return jsonify(books)
+
+
+
+
+@app.route('/booksid', methods=['GET'])
+def api_id():
+    # Check if an ID was provided as part of the URL.
+    # If ID is provided, assign it to a variable.
+    # If no ID is provided, display an error in the browser.
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        return "Error: No id field provided. Please specify an id."
+
+    # Create an empty list for our results
+    results = []
+
+    # Loop through the data and match results that fit the requested ID.
+    # IDs are unique, but other fields might return many results
+    for book in books:
+        if book['id'] == id:
+            results.append(book)
+
+    # Use the jsonify function from Flask to convert our list of
+    # Python dictionaries to the JSON format.
+    return jsonify(results)
+
+#to insert new into data
+@app.route('/insert',methods=['GET','POST'])
+def create():
+    book={'id': 3,
+         'title': 'Secret',
+         'author': 'Rhonda Byrne',
+         'published': '1945'}
+    books.append(book)
+    return jsonify({'Created' : book})
+
+
+
+
+
+app.run()
+
+
